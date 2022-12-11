ae712c9a16b66c13e3c95633d4d89ae14f577477
Hyper-terminal <abhisharma001001001@gmail.com>
2022-12-11 16:43:19 +0530

    movie form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MovieForm from "../../components/UI/MovieForm/MovieForm";
 
 const Home = () => {
-    return <div className="container fw-bold fs-1">Home</div>;
+    return (
+        <div className="container fw-bold fs-1">
+            <MovieForm></MovieForm>
+        </div>
+    );
 };
 
 export default Home;
